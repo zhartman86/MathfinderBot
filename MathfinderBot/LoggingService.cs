96a2d97ec72faa1dd7894b75e10f0cd3bb2d8f9a
zach <zach@DESKTOP-2D9C3V8>
2022-08-02 14:08:28 -0400

    Add project files.

---
+++ MathfinderBot/LoggingService.cs
@@ -0,0 +1,26 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+
+public class LoggingService
+{
+    public LoggingService(DiscordSocketClient client, CommandService commandService)
+    {
+        client.Log          += LogAsync;
+        commandService.Log  += LogAsync;
+    }
+
+    private Task LogAsync(LogMessage msg)
+    {
+        if(msg.Exception is CommandException cmdException)
+        {
+            Console.WriteLine($"[Command/{msg.Severity}] {cmdException.Command.Aliases.First()}"
+            + $" failed to execute in {cmdException.Context.Channel}.");
+            Console.WriteLine(cmdException);
+        }
+        else Console.WriteLine($"[General/{msg.Severity}] {msg}");
+
+        return Task.CompletedTask;
+    }
+}
---
+++ MathfinderBot/Program.cs
@@ -0,0 +1,29 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+public class Program
+{
+    public static Task Main(string[] args) => new Program().MainAsync();
+    
+    
+    private static  CommandService      commandService => new CommandService();
+    private static  DiscordSocketClient client => new DiscordSocketClient();
+
+    public LoggingService logger;
+    
+    
+    public async Task MainAsync()
+    {
+        logger = new LoggingService(client, commandService);
+        
+
+        var token = "MTAwMzg0NDYyODg0MTIzODU4OA.G7kN_9.Q5WgQp222LF52A5_uge958ElOzePLOtNq6TOzo";
+
+        await client.LoginAsync(TokenType.Bot, token);
+        await client.StartAsync();
+        await Task.Delay(-1);
+    }
+
+   
+}

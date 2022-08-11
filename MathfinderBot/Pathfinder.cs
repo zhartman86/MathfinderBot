ff34a771e8c704f19ccc11cc4f82245fa56f93a1
Zachary Hartman <z.hartman86@live.com>
2022-08-10 22:27:43 -0400

    cool stuff

--- MathfinderBot/Pathfinder.cs
+++ MathfinderBot/Pathfinder.cs
@@ -7,6 +7,9 @@ namespace MathfinderBot
     {
         public static Dictionary<IUser, Dictionary<string, StatBlock>>  Database    = new Dictionary<IUser, Dictionary<string, StatBlock>>();       
         public static Dictionary<IUser, StatBlock>                      Active      = new Dictionary<IUser, StatBlock>();               
+    
+        
+
     }
 }
 
--- MathfinderBot/Slash/Var.cs
+++ MathfinderBot/Slash/Var.cs
@@ -1,4 +1,5 @@
-﻿using Discord.Interactions;
+﻿using System.Text.RegularExpressions;
+using Discord.Interactions;
 
 namespace MathfinderBot.Slash
 {
@@ -6,22 +7,81 @@ namespace MathfinderBot.Slash
     {
         public enum VarAction
         {
-
+            SetExpr,
+            SetStat,
+            Remove
         }
         
         public InteractionService Service { get; set; }
+
+        private static Regex ValidVar = new Regex("^[A-Z_]{1,17}$");
 
         private CommandHandler handler;
 
         public Variable(CommandHandler handler) => this.handler = handler;
 
         [SlashCommand("var", "Get a defined stat")]
-        public async Task Var(string var)
+        public async Task Var(VarAction action, string varName, string value)
         {
             var user = Context.Interaction.User;
-            if(!Pathfinder.Active.ContainsKey(user) || Pathfinder.Active[user] == null) { await RespondAsync("No active character", ephemeral: true); return; }
+            if(!Pathfinder.Active.ContainsKey(user) || Pathfinder.Active[user] == null) 
+            { 
+                await RespondAsync("No active character", ephemeral: true); 
+                return; 
+            }
 
-            await RespondAsync(Pathfinder.Active[user][var].ToString());
+            var varToUpper = varName.ToUpper();
+
+            if(!ValidVar.IsMatch(varToUpper))
+            {
+                await RespondAsync("Invalid variable name. A-Z and underscores only. Values will be automatically capitalized.");
+                return;
+            }
+
+            if(action == VarAction.Remove)
+            {
+                if(Pathfinder.Active[user].Stats.ContainsKey(varToUpper))
+                {
+                    Pathfinder.Active[user].Stats.Remove(varToUpper);
+                    await RespondAsync("Value removed from stats.");
+                    return;
+                }
+                else if(Pathfinder.Active[user].Expressions.ContainsKey(varToUpper))
+                {
+                    Pathfinder.Active[user].Expressions.Remove(varToUpper);
+                    await RespondAsync("Value removed from expressions.");
+                    return;
+                }
+
+                await RespondAsync("No variable by that name found.");
+                return;
+            }        
+            
+            if(action == VarAction.SetExpr)
+            {
+                if(Pathfinder.Active[user].Stats.ContainsKey(varToUpper))
+                {
+                    await RespondAsync("Value exists as a stat.");
+                    return;
+                }
+                
+                Pathfinder.Active[user].Expressions[varToUpper] = value;
+            }
+            if(action == VarAction.SetStat)
+            {
+                if(Pathfinder.Active[user].Expressions.ContainsKey(varToUpper))
+                {
+                    await RespondAsync("Value exists as an expression.");
+                    return;
+                }
+                    
+                int val = 0;
+                Pathfinder.Active[user].Stats[varToUpper] = int.TryParse(value, out val) == true ? val : 0;
+            }
+
+            await RespondAsync("Value updated.");
+
+            
         }
     }
 }
